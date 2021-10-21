16730bcf67108d1abb086f47cbf493f4ca2afe59
LehuyH <44150458+LehuyH@users.noreply.github.com>
2021-10-21 16:08:04 -0700

    Handle canvas unfocus

--- src/main.ts
+++ src/main.ts
@@ -9,6 +9,13 @@ import "@/kaboom/scenes/shelter"
 
 (window as any).k = k
 
+//Handle inputs that remain on canvas unfocus 
+k.canvas.addEventListener('blur', ()=>{
+    k.canvas.dispatchEvent(new KeyboardEvent('keyup',{key:"w"}))
+    k.canvas.dispatchEvent(new KeyboardEvent('keyup',{key:"a"}))
+    k.canvas.dispatchEvent(new KeyboardEvent('keyup',{key:"s"}))
+    k.canvas.dispatchEvent(new KeyboardEvent('keyup',{key:"d"}))
+})
 
 //Vue
 const events = mitt()
