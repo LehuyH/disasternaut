179430e26acc216c3f25ccfb04e08911b0d55da2
LehuyH <44150458+LehuyH@users.noreply.github.com>
2021-10-20 19:53:54 -0700

    Add footsteps

--- src/kaboom/objects/player.ts
+++ src/kaboom/objects/player.ts
@@ -1,4 +1,4 @@
-import { Collision, GameObj, KaboomCtx } from "kaboom"
+import { Collision, GameObj, AudioPlay } from "kaboom"
 import k from "@/kaboom"
 import { state } from "@/state"
 import { addBuilding } from "@/kaboom/logic/buildings"
@@ -21,26 +21,51 @@ export default () => [
 
 //Logic
 function behavior() {
+    const dirt = k.play("steps")
+    dirt.loop()
+    dirt.pause()
+    dirt.speed(2)
+
+    const metal = k.play("steps_metal")
+    metal.loop()
+    metal.pause()
+metal.speed(1.5)
+    
     const playAnim = (player: any, name: string) => {
         if (name != player.curAnim()) {
             player.play(name)
         }
     }
 
+    const playSteps = ()=>{
+        if(state.scene === "planet"){
+            dirt.play()
+            metal.stop()
+        }
+        else{
+            metal.play()
+            dirt.stop()
+        }
+    }
+
     const movement = {
         'down': (p: GameObj<any>) => {
             p.move(0, 200)
+            playSteps()
         },
         'up': (p: GameObj<any>) => {
             p.move(0, -200)
+            playSteps()
         },
         'left': (p: GameObj<any>) => {
             p.move(-200, 0)
             playAnim(p, "left")
+            playSteps()
         },
         'right': (p: GameObj<any>) => {
             p.move(200, 0)
             playAnim(p, "left")
+            playSteps()
         }
     }
 
@@ -126,6 +151,10 @@ function behavior() {
             else if (k.keyIsDown("a") || k.keyIsDown("left")) movement.left(this)
             else if (k.keyIsDown("w") || k.keyIsDown("up")) movement.up(this)
             else if (k.keyIsDown("s") || k.keyIsDown("down")) movement.down(this)
+            else{
+                metal.stop()
+                dirt.stop()
+            }
 
 
         },
@@ -159,5 +188,7 @@ k.loadSprite("player", "sprites/player.png", {
     }
 })
 k.loadSprite("axe", "sprites/axe.png")
+k.loadSound("steps_metal","audio/steps_metal.webm")
+k.loadSound("steps","audio/steps.webm")
 
 
